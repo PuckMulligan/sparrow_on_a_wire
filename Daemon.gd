c93cb831582f0b33df2e94d0807caa52f3e4ce9f
Daniel Steinberg <daniel.marc.ballard@gmail.com>
2024-01-06 11:46:32 -0500

    index on running_dict_active: 20d795f babaispushalwayson

--- Daemon.gd
+++ Daemon.gd
@@ -18,15 +18,23 @@ func _physics_process(delta):
 	elif Input.is_action_just_pressed("ui_up"):
 		direction.y -= 1
 
+	move_and_push_block(direction)
+
+func move_and_push_block(direction):
 	var new_position = global_position + direction * get_parent().grid_size
 	var bodies = get_world_2d().direct_space_state.intersect_point(new_position)
 
 	if bodies.size() == 0:
 		global_position = new_position
-	elif bodies.size() == 1 and bodies[0].collider is Block and "baba" in get_parent().rules and get_parent().rules["baba"] == "push":
+	elif bodies.size() == 1 and bodies[0].collider is Block and is_pushable(bodies[0].collider): #debated is_pushable or is_push. decided we'll check for pushable bc
+																									#might wanna add other logic to check if a block is pushable later
 		var block_new_position = bodies[0].collider.global_position + direction * get_parent().grid_size
 		var block_bodies = get_world_2d().direct_space_state.intersect_point(block_new_position)
 
 		if block_bodies.size() == 0:
 			bodies[0].collider.global_position = block_new_position
 			global_position = new_position
+
+func is_pushable(block):
+	#if block is currently push
+	return get_parent().is_rule_active("block","push")
